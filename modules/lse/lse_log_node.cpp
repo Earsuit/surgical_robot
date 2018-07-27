f12c09d5eb5de46a450069bdf172801859430812
earsuit <renyudicn@outlook.com>
2018-07-27 11:56:09 +1000

    lse_log_node test passed

--- modules/lse/lse_log_node.cpp
+++ modules/lse/lse_log_node.cpp
@@ -1,6 +1,7 @@
 #include "ros/ros.h"
 #include <stdlib.h>
 #include <stdio.h>
+#include <string>
 #include <boost/bind.hpp>
 #include <boost/ref.hpp>
 #include "surgical_robot/system_identification.h"
@@ -14,16 +15,34 @@ int main (int argc, char **argv){
     ros::init(argc,argv,"lse_log");
     ros::NodeHandle n;
     
-    //file
+    //file, audomatically increase
     system("mkdir -p ${HOME}/catkin_ws/src/surgical_robot/modules/lse/data");
-    FILE* fp;
-    fp = fopen("/data/data.csv","w");
+    int num = 0;
+    string prefix = "data_";
+    string suffix = ".csv";
+    string name;
+    string dir = "/home/earsuit/catkin_ws/src/surgical_robot/modules/lse/data/";
+    string path;
+    FILE* fp = NULL;
+    do{
+        if(fp !=NULL)
+            fclose(fp);     
+        num++;
+        name = prefix + to_string(num)+suffix;
+        path = dir+name;
+        ROS_DEBUG("%s",path.c_str());
+        fp = fopen(path.c_str(),"r");
+    }while(fp != NULL);
+    fp = fopen(path.c_str(),"w");
+    fprintf(fp,"Angle,current,Voltage\n");
 
     //subscriber 
     sub_callback subscriber_callback = boost::bind(subscriberCallback,fp,_1);
     ros::Subscriber lse_log_sub = n.subscribe("system_identification",1000,subscriber_callback);
 
     ros::spin();
+
+    fclose(fp);
 
     return 0;
 }
