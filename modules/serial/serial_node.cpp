65254f13ad47f7ff5fc8bc4d618f678b8d0d5edd
Earsuit <renyudicn@outlook.com>
2018-09-16 21:54:41 +1000

    change the motor command to angles

--- modules/serial/serial_node.cpp
+++ modules/serial/serial_node.cpp
@@ -12,22 +12,15 @@
 
 #define DEFAULT_RATE 20
 #define DEFAULT_BAUDRATE 115200
-#define RX_BUFFER_SIZE 35
-#define TX_BUFFER_SIZE 10
+#define TX_BUFFER_SIZE 18
 #define PACKAGE_HEAD 0x51
 #define PACKAGE_TAIL 0x71
-#define RX_PACKAGE_LEN 18
-#define NOT_FOUND -1
 #define RATE_LOWER_LIMIT 1
 #define MOTOR_FEEDBACK_LENGTH 4 
-#define MOTOR_COMMANDS_LENGTH 8
+#define MOTOR_COMMANDS_LENGTH 4
 
-int findPackage(uint8_t* buffer,int size);
-void subscriberCallback(int port,const surgical_robot::motor_commandsConstPtr &);
+void subscriberCallback(uint8_t* buffer,int port,const surgical_robot::motor_commandsConstPtr &);
 typedef const boost::function<void(const surgical_robot::motor_commandsConstPtr & )> sub_callback;
-
-void publisherCallback(int ,uint8_t*,ros::Publisher&,surgical_robot::motor_feedback &,const ros::TimerEvent&);
-typedef const boost::function<void(const ros::TimerEvent&)> pub_callback;
 
 int main(int argc, char** argv){
     ros::init(argc,argv,"serial");
@@ -43,18 +36,13 @@ int main(int argc, char** argv){
         ROS_ERROR("Can not open comport");
         return 0;
     }
-    uint8_t buffer[RX_BUFFER_SIZE];
 
-    //publisher
-    ros::Publisher motor_feedback_pub = n.advertise<surgical_robot::motor_feedback>("motor_feedback",1000);
-    surgical_robot::motor_feedback msg;
-    msg.motor_1 = msg.motor_2 = msg.motor_3 = msg.motor_4 =0;
-    pub_callback publisher_callback =  boost::bind(publisherCallback,atoi(argv[3]),buffer,boost::ref(motor_feedback_pub),boost::ref(msg),_1);
-    ros::Timer timer = n.createTimer(ros::Duration(rate),publisher_callback);
-
+    uint8_t buffer[TX_BUFFER_SIZE];
+    buffer[0] = PACKAGE_HEAD;
+    buffer[TX_BUFFER_SIZE-1] = PACKAGE_TAIL;
     //subscriber
-    sub_callback sub_callback = boost::bind(subscriberCallback,atoi(argv[3]),_1);
-    ros::Subscriber motor_command_pub = n.subscribe("motor_command",1000,sub_callback);
+    sub_callback sub_callback = boost::bind(subscriberCallback,buffer,atoi(argv[3]),_1);
+    ros::Subscriber motor_command_pub = n.subscribe("motor_command",100,sub_callback);
 
     // using two threads 
     ros::AsyncSpinner s(2);
@@ -64,38 +52,9 @@ int main(int argc, char** argv){
     return 0;
 }
 
-int findPackage(uint8_t* buffer,int size){
-    if(size>=RX_PACKAGE_LEN){
-        for(int i=0;i<=(size-RX_PACKAGE_LEN);i++){
-            ROS_DEBUG("BYTE: %x",buffer[i]);
-            if(buffer[i] == PACKAGE_HEAD && buffer[i+RX_PACKAGE_LEN-1] == PACKAGE_TAIL){
-                return i+1;
-            }
-        }
-    }
-    return NOT_FOUND;
-}
-
-void subscriberCallback(int port,const surgical_robot::motor_commandsConstPtr &msg){
-    ROS_INFO("Commands received: %d, %d, %d, %d, %d, %d, %d, %d", msg->motor_1_v,msg->motor_1_dir,msg->motor_2_v,msg->motor_2_dir,msg->motor_3_v,msg->motor_3_dir,msg->motor_4_v,msg->motor_4_dir);
-    uint8_t buffer[TX_BUFFER_SIZE];
-    int sizef = sizeof(msg->motor_1_v);
-    buffer[0] = PACKAGE_HEAD;
-    memcpy(buffer+1,&msg->motor_1_v,MOTOR_COMMANDS_LENGTH*sizef);
-    buffer[TX_BUFFER_SIZE-1] = PACKAGE_TAIL;
+void subscriberCallback(uint8_t* buffer,int port,const surgical_robot::motor_commandsConstPtr &msg){
+    ROS_INFO("Commands received: %f, %f, %f, %f", msg->motor_1,msg->motor_2,msg->motor_3,msg->motor_4);
+    int sizef = sizeof(msg->motor_1);
+    memcpy(buffer+1,&msg->motor_1,MOTOR_COMMANDS_LENGTH*sizef);
     RS232_SendBuf(port,buffer,TX_BUFFER_SIZE);
-}
-
-void publisherCallback(int port,uint8_t* buffer,ros::Publisher& pub,surgical_robot::motor_feedback & msg,const ros::TimerEvent& timer){
-    int numOfBytes = RS232_PollComport(port,buffer,RX_BUFFER_SIZE);
-    ROS_DEBUG("Received: %d",numOfBytes);
-    int index = findPackage(buffer,numOfBytes);
-    int sizef = sizeof(float);
-    if(index!=NOT_FOUND){
-        memcpy(&msg.motor_1,buffer+index,MOTOR_FEEDBACK_LENGTH*sizef);
-    }else
-        ROS_WARN("Package not found!");
-    
-    ROS_INFO("%f, %f, %f, %f",msg.motor_1,msg.motor_2,msg.motor_3,msg.motor_4);
-    pub.publish(msg);
 }
