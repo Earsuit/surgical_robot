fc284ad6fec82fd5a132e11cc1ddcc4f8ae015bb
Earsuit <renyudicn@outlook.com>
2018-10-15 13:40:42 +1100

    change int to float in openloop

--- modules/openloop/openloop_node.cpp
+++ modules/openloop/openloop_node.cpp
@@ -38,14 +38,24 @@ int main(int argc, char** argv){
 
 void subscriberCallback(ros::Publisher& motor_commands_pub,surgical_robot::motor_commands &msg, float* angle,const surgical_robot::joystick_readingConstPtr & joystick){
     ROS_INFO("Joystick reading received: %d, %d, %d, %d", joystick->joystick_1_x,joystick->joystick_1_y,joystick->joystick_2_x,joystick->joystick_2_y);
-    angle[0] += ((joystick->joystick_1_x > MIDDLE_1_x)?joystick->joystick_1_x%MIDDLE_1_x:-joystick->joystick_1_x%MIDDLE_1_x)/SCALE;
-    angle[1] += ((joystick->joystick_1_y > MIDDLE_1_y)?joystick->joystick_1_y%MIDDLE_1_y:-joystick->joystick_1_y%MIDDLE_1_y)/SCALE;
-    angle[2] += ((joystick->joystick_2_x > MIDDLE_2_x)?joystick->joystick_2_x%MIDDLE_2_x:-joystick->joystick_2_x%MIDDLE_2_x)/SCALE;
+    x1 = joystick->joystick_1_x;
+    y1 = joystick->joystick_1_y;
+    x2 = joystick->joystick_2_x;
+    y2 = joystick->joystick_2_y;
+
+    x1Diff = (abs(x1 - MIDDLE_1_x)<5) ? 0 : x1 - MIDDLE_1_x;
+    y1Diff = (abs(y1 - MIDDLE_1_y)<5) ? 0 : y1 - MIDDLE_1_y;
+    x2Diff = (abs(x2 - MIDDLE_2_x)<5) ? 0 : x2 - MIDDLE_2_x;
+    y2Diff = (abs(y2 - MIDDLE_2_y)<5) ? 0 : y2 - MIDDLE_2_y; 
+    
+    angle[0] += ( (x1Diff >= 0) ? x1Diff : -x1Diff ) / SCALE;
+    angle[1] += ( (y1Diff >= 0) ? y1Diff : -y1Diff ) / SCALE;
+    angle[2] += ( (x2Diff >= 0) ? x2Diff : -x2Diff ) / SCALE;
 
     msg.motor_1 = angle[0];
     msg.motor_2 = angle[1];
     msg.motor_3 = angle[2];
-    msg.motor_4 = ((joystick->joystick_2_y > MIDDLE_2_y)?(joystick->joystick_2_y%MIDDLE_2_y)*MAX/MIDDLE_2_y:(-joystick->joystick_2_y%MIDDLE_2_y)*MAX/MIDDLE_2_y);
+    msg.motor_4 = (y2Diff >= 0) ? y2Diff*MAX/MIDDLE_2_y : -y2Diff*MAX/MIDDLE_2_y;
     ROS_INFO("Motor commands: %f, %f, %f, %f",msg.motor_1,msg.motor_2,msg.motor_3,msg.motor_4);
     motor_commands_pub.publish(msg);
 }
